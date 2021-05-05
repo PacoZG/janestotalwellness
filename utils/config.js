fcf6f8077f2529eed83175439d0c6258366f5846
Francisco Zavala <64326252+PacoZG@users.noreply.github.com>
2021-05-05 14:43:11 +0300

    update few files

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 let MONGODB_URI = process.env.MONGODB_URI
 
-console.log('NODE_ENV === ', process.env.NODE_ENV)
+// console.log('NODE_ENV === ', process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
